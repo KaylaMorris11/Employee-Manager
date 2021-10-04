054d8ac80fe5dc4059957a662ccd2e9b9f674df9
Kayla Morris <moreiskay14@gmail.com>
2021-10-03 17:19:19 -0700

    Updated schema file to include foreign keys in tables

--- db/schema.sql
+++ db/schema.sql
@@ -20,6 +20,9 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department (id_department)
+    ON DELETE CASCADE
 );
 --
 
@@ -30,5 +33,11 @@ id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id: INT NOT NULL,
-manger_id INT
+manger_id INT,
+FOREIGN KEY (role_id) 
+REFERENCES role(id_role) 
+ON DELETE CASCADE,
+FOREIGN KEY (manager_id) 
+REFERENCES employee(id_employee) 
+ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
     password: 'password',
     database: 'employee_db'
   },
-  console.log(`Connected to the movies_db database.`)
+  console.log(`Connected to the employee_db database.`)
 );
 
 const chooseAnOption = [{
