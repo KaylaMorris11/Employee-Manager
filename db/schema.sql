3ef85896213a12e0732bf2218bc3f645d4dd06c9
Kayla Morris <moreiskay14@gmail.com>
2021-10-03 19:40:04 -0700

    Fixed errors in mysql on schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 -- ----------------------------------------------------------------
 
@@ -15,9 +15,9 @@ CREATE TABLE roles(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE CASCADE
+     FOREIGN KEY (department_id) 
+     REFERENCES department(id)
+     ON DELETE CASCADE
 );
 -- ----------------------------------------------------------------
 
@@ -25,12 +25,12 @@ CREATE TABLE employees (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
+roles_id INT NOT NULL,
 manager_id INT,
-FOREIGN KEY (role_id) 
-REFERENCES role (id_role) 
+FOREIGN KEY (roles_id)
+REFERENCES roles(id)
 ON DELETE CASCADE,
-FOREIGN KEY (manager_id) 
-REFERENCES employee(id_employee) 
+FOREIGN KEY (manager_id)
+REFERENCES employees(id)
 ON DELETE SET NULL
 );
